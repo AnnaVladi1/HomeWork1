7bbf2d5ecfa53e56db3a8cab115eb6192005759f
Anna <anuyta3091@mail.ru>
2022-12-02 16:45:58 +0300

    Homework 5

---
+++ HomeWork5.js
@@ -0,0 +1,15 @@
+let arr = [3, 5, 7, 9, 15];
+
+function massive(arr) {
+
+    console.log(`Длинна массива - ${arr.length}`);
+
+    arr.map(function(item) {
+
+        console.log(item);
+
+    });
+
+}
+
+massive(arr);
